06b8e6e9106da3378dd655b71e0297b30bedf3e8
Anton Aliaksandrau <antonalex2021@gmail.com>
2026-04-24 20:13:39 +0300

    solve task02

---
+++ Task02/logic.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define DEF_SIZE 100
+
+int count_negative_elements(int array[], int size);
---
+++ Task02/util.h
@@ -0,0 +1,6 @@
+#include "logic.h"
+
+using namespace std;
+
+void init_random(int array[], int size, int a, int b);
+string convert(int array[], int size);
